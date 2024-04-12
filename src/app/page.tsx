516749da88a570683fa09f6fc4113557ad1cea12
Atai Barkai <atai.barkai@gmail.com>
2024-04-12 00:45:57 -0700

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ import { INSTRUCTIONS } from "./instructions";
 
 const HomePage = () => {
   return (
+    
     <CopilotKit url="/api/copilotkit">
       <CopilotSidebar
         instructions={INSTRUCTIONS}
